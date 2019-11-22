0c3d7613adeec0735bc20fe9d57092fa2d9feebb
Mike Nguyen <peps.minhnguyen@gmail.com>
2019-11-22 14:32:39 -0500

    create the component skeleton to kick off the project

---
+++ src/components/join.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Join() {
+   return (
+     <div>
+       <h2>Join</h2>
+       <h3>add question</h3>
+       <h3>add question bar</h3>
+     </div>
+   );
+ }
---
+++ src/components/main.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Main () {
+   return (
+     <div>
+       <h2>Main</h2>
+       <h3>CHAT ROOM</h3>
+       <h3>TABLE QuESTION</h3>
+     </div>
+   );
+ }
